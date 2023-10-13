59537090a7a05c3742eb8ceccf3f0b83291a8382
Kurstan <kurstanerkinbaev2@gmail.com>
2023-10-13 17:53:12 +0300

    initial commit

---
+++ src/main/java/com/example/iqro/IqroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.iqro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IqroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IqroBackendApplication.class, args);
+	}
+
+}
