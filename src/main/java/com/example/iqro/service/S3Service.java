77b82ca52bc5d3392b5af18783252bc876a753c4
ZHIIDEGUL <emranlini@gmail.com>
2023-10-26 13:03:49 +0600

    S3 config

---
+++ src/main/java/com/example/iqro/service/S3Service.java
@@ -0,0 +1,15 @@
+package com.example.iqro.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Map;
+
+public interface S3Service {
+    Map<String, String> uploadFile(MultipartFile file) throws IOException;
+
+    Map<String, String> deleteFile(String fileLink);
+
+    String uploadImage(BufferedImage image, String fileName) throws IOException;
+}
